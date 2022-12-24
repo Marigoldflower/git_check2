ce2133508c3f0fdb9cfceb0394f5d22c6f5df601
황홍필 <marigold410@naver.com>
2022-12-24 15:21:53 +0900

    Initial Commit

---
+++ git_check2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  git_check2
+//
+//  Created by 황홍필 on 2022/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
